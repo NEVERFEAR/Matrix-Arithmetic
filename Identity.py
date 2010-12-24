309ae5be71372e7792e53f9bda88f04729d7ba23
Doug <doug@neverfear.org>
2010-12-24 20:26:15 +0000

    Created identity sample

---
+++ Identity.py
@@ -0,0 +1,14 @@
+from MatrixArithmetic import *
+"""
+An example of the Identity function
+"""
+
+M = [
+        [ 1, 2,  4],
+        [ 1, 3,  9],
+        [ 1, 7, 49]
+]
+
+print ToString(M)
+print ToString(Identity(M), Name = "I")
+
